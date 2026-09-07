c2d598d46b7a26c4611d9e2dceb0e7782714fda0
mcc <mcc@mcc.mcc>
2026-09-07 10:22:10 +0800

    build React page components

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+      <footer>
+        <small>© 20xx martin development. All rights reserved.</small>
+      </footer>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,21 @@
 import { createRoot } from "react-dom/client"
-import reactLogo from './assets/react.svg'
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 const root = createRoot(document.getElementById("root"))
 
-/**
- * Challenge (part 1):
- * Create a custom "Page" component
- * 
- * It should return an ordered list with the reasons why you're
- * excited to be learning React :)
- * 
- * Render the Page component.
- */
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
 
 root.render(
     <Page/>
 )
 
-function Page() {
-  return (
-    <div>
-      <img src={reactLogo} alt="React logo" />
-      <h1>Why React is exciting</h1>
-      <ul>
-        <li>super easy</li>
-        <li>super fast</li>
-        <li>super fun</li>
-      </ul>
-    </div>
-  )
-}
